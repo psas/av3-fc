eec45329522c057c4ad5d18275b0ac790b4ca594
Ian Osgood <iano@quirkster.com>
2012-10-13 11:51:26 -0700

    Implement packet parsing (needs testing).

--- gps-gsource.c
+++ gps-gsource.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdint.h>
 #include <glib.h>
 #include "logging.h"
 #include "crescent.h"
@@ -7,15 +8,38 @@
 
 #define GPS_CODE FOURCC('G','P','S','U')
 
-static gchar *has_packet(gchar *buf, size_t size, size_t *len)
+static void *memstr(const void *m, const char *s, size_t len)
 {
-	/* find "$BIN" in buf+size */
-	/* parse out packet length */
+	size_t l = strlen(s);
+	void *p;
+	while ((p = memchr(m, *s, len)) != NULL)
+	{
+		len -= p-m + 1;
+		if (len <= l)
+			return NULL;
+		if (memcmp(p, s, l) == 0)
+			return p;
+		m = p + 1;
+	}
+	return p;
+}
+
+static gchar *has_packet(gchar *buf, size_t size, uint16_t *len)
+{
+	gchar *p = memstr(buf, "$BIN", size);
+	if (p == NULL)
+		return NULL;
+
+	uint16_t packet_len;
+	memcpy(&packet_len, p+6, 2);
 	/* check whether we have that much data, and if it ends with \r\n */
-	/* if so, return found $BIN and length of data */
+	if (size < packet_len + 12)
+		return NULL;
+	if (memcmp(p+packet_len+2, "\r\n", 2) != 0)
+		return NULL;		//!!! what to do?
 	/* TODO: checksum? return checksum out-of-band? */
-	/* return NULL if packet is incomplete */
-	return NULL;
+	*len = packet_len + 12;
+	return p;
 }
 
 static gchar buf[4096], *cur = buf;
@@ -29,16 +53,15 @@ static gboolean read_gps_cb(GIOChannel *gps, GIOCondition cond, gpointer data)
         g_io_channel_read_chars(gps, cur, sizeof(buf)-(cur-buf), &nread, NULL);
 	cur += nread;
 	
-	size_t len = 0;
+	uint16_t len = 0;
 	gchar *packet;
 	while ((packet = has_packet(buf, cur-buf, &len)))
 	{
-		write_tagged_message(GPS_CODE, packet, len-2);
+		write_tagged_message(GPS_CODE, packet, len);
 		memcpy(buf, packet+len, cur-packet+len);
 		cur = buf + (cur-packet+len);  
 	}
-
-        return TRUE;
+	return TRUE;
 }
 
 void init_gps(void)
