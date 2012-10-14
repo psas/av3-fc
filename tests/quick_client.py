fb8d553476ac5f9e7c3da04e3e01215b2a0c12a9
Jamey Sharp <jamey@minilop.net>
2012-10-13 17:24:10 -0700

    quick_client.py: Incorporate Crescent GPS packet decoding.

--- tests/quick_client.py
+++ tests/quick_client.py
@@ -4,12 +4,21 @@ the (probably fake) flight comptuer and read
 the data as a smoke test"""
 
 import socket
+import struct
 
 from fc_proto import packet_header, tag_header
 
 # UDP packets on localhost
 UDP_IP   = ""
 UDP_PORT = 5005
+
+message_types = {
+    'GPS\x01': struct.Struct("<BBHdddfffffHH H"),
+    'GPS\x4C': struct.Struct("<dH8x 81L 15L 12L H"),
+    'GPS\x5F': struct.Struct("<HxxL 10L 10L 10L H"),
+    'GPS\x60': struct.Struct("<xxHd " + ''.join(("BBBB" for i in range(12))) + "12L 12d 12d H"),
+    'GPS\x62': struct.Struct("<" + ''.join(("HBBBBbB" for i in range(8))) + "BBxx H"),
+}
 
 # Open socket and bind to address
 sock = socket.socket(socket.AF_INET,
@@ -32,4 +41,7 @@ while 1:
         message = message[length:]
 
         timestamp = timestamp_hi << 32 | timestamp_lo
+        decoder = message_types.get(fourcc)
+        if decoder is not None and len(body) == decoder.size:
+            body = decoder.unpack(body)
         print timestamp, repr(fourcc), repr(body)
