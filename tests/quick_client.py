bffb51762265bf65c0b4e2d8473c7846815e1938
Jamey Sharp <jamey@minilop.net>
2012-10-13 17:42:48 -0700

    Semi-decode some more Crescent GPS messages.

--- tests/quick_client.py
+++ tests/quick_client.py
@@ -14,10 +14,13 @@ UDP_PORT = 5005
 
 message_types = {
     'GPS\x01': struct.Struct("<BBHdddfffffHH H"),
+    'GPS\x02': struct.Struct("<LLHHHH H"),
     'GPS\x4C': struct.Struct("<dH8x 81L 15L 12L H"),
     'GPS\x5F': struct.Struct("<HxxL 10L 10L 10L H"),
     'GPS\x60': struct.Struct("<xxHd " + ''.join(("BBBB" for i in range(12))) + "12L 12d 12d H"),
+    'GPS\x61': struct.Struct("<L 4H 3L 2H H"),
     'GPS\x62': struct.Struct("<" + ''.join(("HBBBBbB" for i in range(8))) + "BBxx H"),
+    'GPS\x63': struct.Struct("<BBHd" + ''.join(("8B b 3B H 5h" for i in range(12))) + "hH H"),
 }
 
 # Open socket and bind to address
