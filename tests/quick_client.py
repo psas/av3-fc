a4a50f7ba71ff0dd93a6a154f2a41b4a63c88f08
Jamey Sharp <jamey@minilop.net>
2012-10-14 21:02:50 -0700

    Add a utility for pretty-printing a flight computer log file.

--- tests/quick_client.py
+++ tests/quick_client.py
@@ -4,24 +4,12 @@ the (probably fake) flight comptuer and read
 the data as a smoke test"""
 
 import socket
-import struct
 
-from fc_proto import packet_header, tag_header
+from fc_proto import packet_header, tag_header, message_types
 
 # UDP packets on localhost
 UDP_IP   = ""
 UDP_PORT = 5005
-
-message_types = {
-    'GPS\x01': struct.Struct("<BBHdddfffffHH H"),
-    'GPS\x02': struct.Struct("<LLHHHH H"),
-    'GPS\x4C': struct.Struct("<dH8x 81L 15L 12L H"),
-    'GPS\x5F': struct.Struct("<HxxL 10L 10L 10L H"),
-    'GPS\x60': struct.Struct("<xxHd " + ''.join(("BBBB" for i in range(12))) + "12L 12d 12d H"),
-    'GPS\x61': struct.Struct("<L 4H 3L 2H H"),
-    'GPS\x62': struct.Struct("<" + ''.join(("HBBBBbB" for i in range(8))) + "BBxx H"),
-    'GPS\x63': struct.Struct("<BBHd" + ''.join(("8B b 3B H 5h" for i in range(12))) + "hH H"),
-}
 
 # Open socket and bind to address
 sock = socket.socket(socket.AF_INET,
