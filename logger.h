d5a4a8903d6bbaf5c3901ee03cd3f1195bb556be
buckwheat24 <clark.wachsmuth@gmail.com>
2013-06-09 00:00:21 -0700

    Made log filename dynamically generated to discourage overwriting of older data. Packages log data up to MTU of 1500 and then sends to disk.

--- logger.h
+++ logger.h
@@ -1,8 +1,9 @@
 #ifndef _LOGGER_H_
 #define _LOGGER_H_
 
-#include "psas_packet.h"
+//#include "psas_packet.h"
 void logger_init(void); // [miml:init]
 void logger_final(void); // [miml:final]
-extern void log_getPositionData_adis(ADIS_packet*); // [miml:receiver]
+extern void log_getPositionData_adis(unsigned char *, int); // [miml:receiver]
+//extern void log_getPositionData_adis(ADIS_packet*); 
 #endif
