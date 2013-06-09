f5a3e167807b8564eda78a2c29471a2c9b0bc038
buckwheat24 <clark.wachsmuth@gmail.com>
2013-06-08 23:58:20 -0700

    Removed ADIS packet references, just sends char array pointer. Removed superfulous init.

--- adis.h
+++ adis.h
@@ -6,9 +6,9 @@
 
 #ifndef VIRTDEVSRV_H_
 #define VIRTDEVSRV_H_
-#include "psas_packet.h"
+//#include "psas_packet.h"
 extern int adis_init(void); // [miml:init]
 extern void adis_final(void); // [miml:final]
-extern void log_getPositionData_adis(ADIS_packet*); // [miml:sender]
+extern void sendADISData(unsigned char *, int); // [miml:sender]
 
 #endif /* VIRTDEVSRV_H_ */
