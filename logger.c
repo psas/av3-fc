801a84a44d26a237097aa1214e5f5931b9445148
buckwheat24 <clark.wachsmuth@gmail.com>
2013-06-09 16:07:35 -0700

    Adds new line feed. Need to still check if there is an old log file with same name and add offset.

--- logger.c
+++ logger.c
@@ -13,11 +13,14 @@
 #include "utils_sockets.h"
 
 #define P_LIMIT 1500
+#define WIFI_IP "192.168.0.20"
+#define WIFI_PORT 36000
 
 static FILE *fp = NULL;
 static char filename[50];
 static char log_buffer[1500]; 		// Global so destructor can flush final data
 static int log_buffer_size = 0;
+static int sd;
 
 
 void logger_init() {
@@ -27,6 +30,7 @@ void logger_init() {
 		fprintf (stderr, "disk logger: could not open file %s for writing: %s\n", filename, strerror(errno));
 	}
 	setbuf(fp, NULL);
+	sd = get_send_socket();
 }
 
 
@@ -42,10 +46,13 @@ void  log_getPositionData_adis(unsigned char *data, int size) {
 	if(log_buffer_size + size >= P_LIMIT){
 		// Send current buffer to disk/wifi
 		fwrite(log_buffer, sizeof(char), log_buffer_size, fp);
+		sendto_socket(sd, log_buffer, log_buffer_size, WIFI_IP, WIFI_PORT);
 		log_buffer_size = 0;
 	}
 
 	memcpy(&log_buffer[log_buffer_size], data, size);
 	log_buffer_size += size;
+	memcpy(&log_buffer[log_buffer_size], "\n", 1);
+	log_buffer_size++;
 	printf("size: %d (+ %d) \n", log_buffer_size, size);
 }
