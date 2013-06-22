172ffc4ed1216f1dc7b42d22bcb40b4a423af15d
Nathan Bergey <nathan.bergey@gmail.com>
2013-06-22 04:27:50 -0700

    Finished working sim of ADIS data that runs through FCF with correct packet output from the logger

--- module_adis_sim.c
+++ module_adis_sim.c
@@ -42,10 +42,24 @@ static void adis_sim_cb (struct pollfd * pfd) {
 		if (fread(&cr, sizeof(char), 1, stream) == 1) {
 
 			// tmp
-			char line[1024];
+			char line[128];
+
+			// Unpack data
 			ADIS16405_burst_data data;
 			data.adis_supply_out = values_unpack[0];
+			data.adis_xgyro_out = values_unpack[1];
+			data.adis_ygyro_out = values_unpack[2];
+			data.adis_zgyro_out = values_unpack[3];
+			data.adis_xaccl_out = values_unpack[4];
+			data.adis_yaccl_out = values_unpack[5];
+			data.adis_zaccl_out = values_unpack[6];
+			data.adis_xmagn_out = values_unpack[7];
+			data.adis_ymagn_out = values_unpack[8];
+			data.adis_zmagn_out = values_unpack[9];
+			data.adis_temp_out = values_unpack[10];
+			data.adis_aux_adc = values_unpack[11];
 
+			// dump data into socket
 			memcpy(line, &data, sizeof(ADIS16405_burst_data));
 			sendto_socket(sd, line, sizeof(ADIS16405_burst_data), FC_IP, FC_LISTEN_PORT);
 		}
@@ -61,7 +75,8 @@ void init_profiling() {
 	t.it_interval.tv_sec = 0;
 	t.it_interval.tv_nsec = 0;
 	t.it_value.tv_sec = 0;
-	t.it_value.tv_nsec = 300000;
+	t.it_value.tv_nsec = 1220703; // 819.2 samples per second
+	//t.it_value.tv_nsec = 50000000; // Super slow, for debug
 	fd = timerfd_create(CLOCK_MONOTONIC, 0);
 	timerfd_settime(fd, 0, &t, NULL);
 
