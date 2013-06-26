fe7708531b04f170a3663f6b407cda1af5bcc3ab
TheoH <Theo0x48@gmail.com>
2013-06-26 02:28:45 -0700

    Started rc khz rate conversion

--- rollcontrol.c
+++ rollcontrol.c
@@ -1,34 +1,35 @@
+#include <stdio.h>
 #include <stdbool.h>
+#include <sys/timerfd.h>
+#include "fcfutils.h"
 #include "rollcontrol.h"
 #include "rollControlLibrary.h"
-#include <stdio.h>
+
 static bool launch;
+static uint16_t accel;
+static uint16_t roll;
 
-void rollcontrol_init(void){
-	launch = 0;
-	rc_init();
-}
-
-uint16_t scale_accel(int16_t acc){
+static uint16_t scale_accel(int16_t acc){
 	const double shift = 34500 + 1374.05;
 	const double scale = 4.57559; // (0.003 g/adis_lsb) * (1374.05 rc_counts/g)
 	return (uint16_t)(((double)-acc)*scale + shift);
 }
 
-uint16_t scale_gyro(int16_t gyr){
+static uint16_t scale_gyro(int16_t gyr){
 	const double shift = 29400;
 	const double scale = 2.1845;// (0.05 deg/sec/adislsb) * (43.69 rc_counts/deg/sec)
 	return (uint16_t)(((double)-gyr)*scale + shift);
 }
-void rc_receive_imu(ADISMessage * imu){
+
+static void step(){
 	RC_INPUT_STRUCT_TYPE input;
 	RC_OUTPUT_STRUCT_TYPE output;
 
-	input.u16RawAccelerometerADC = scale_accel(imu->data.adis_xaccl_out);
-	input.u16RawRateGyroADC = scale_gyro(imu->data.adis_xgyro_out);
+	input.u16RawAccelerometerADC = accel;
+	input.u16RawRateGyroADC = roll;
 	input.u8IsLaunchDetected = launch;
+
 	rc_step(&input, &output);
-	output.u8ServoDisableFlag=0;
 
 	RollServoMessage out = {
 			.ID = {"ROLL"},
@@ -39,6 +40,26 @@ void rc_receive_imu(ADISMessage * imu){
 	};
 
 	rc_send_servo(&out);
+}
+
+void rollcontrol_init(void){
+	launch = 0;
+	accel = scale_accel(0);
+	roll = scale_gyro(0);
+	rc_init();
+	int tfd = timerfd_create(CLOCK_MONOTONIC, TFD_NONBLOCK);
+	struct itimerspec  newval;
+	newval.it_interval.tv_sec = 0;
+	newval.it_interval.tv_nsec = 1000000; //1 ms
+	newval.it_value.tv_sec = 60*60*24*7; // TODO: re-add a new tfd when this finally expires
+	newval.it_value.tv_nsec = 0;
+	timerfd_settime(tfd, 0, &newval, NULL);
+	fcf_add_fd(tfd, POLLIN, step);
+}
+
+void rc_receive_imu(ADISMessage * imu){
+	accel = scale_accel(imu->data.adis_xaccl_out);
+	roll = scale_gyro(imu->data.adis_xgyro_out);
 }
 
 void rc_receive_arm(char * signal){
