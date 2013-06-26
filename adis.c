8017a8af174d1fadca0416a8682e2ce77ec71a5b
TheoH <Theo0x48@gmail.com>
2013-06-26 00:34:11 -0700

    Really fixed adis endian and twos complement issues

--- adis.c
+++ adis.c
@@ -18,9 +18,13 @@
  *  32/33 = TEMP - 12bit
 */
 
-int16_t fix_complement(uint16_t value, int bits) {
-	int distance = 16 - bits;
-	return ((int16_t)value << distance) >> distance;
+int16_t fix_complement(uint16_t val, int bits) {
+	if((val&(1<<(bits-1))) != 0){
+		val = val - (1<<bits);
+	}
+	return val;
+//	int distance = 16 - bits;
+//	return ((int16_t)value << distance) >> distance;
 }
 
 void adis_raw_in(unsigned char *buffer, int len, unsigned char* timestamp) {
@@ -31,21 +35,21 @@ void adis_raw_in(unsigned char *buffer, int len, unsigned char* timestamp) {
 				.timestamp={(uint8_t)timestamp[0], (uint8_t)timestamp[1],
 						    (uint8_t)timestamp[2], (uint8_t)timestamp[3],
 						    (uint8_t)timestamp[4], (uint8_t)timestamp[5]},
-				.data_length=buffer[10] << 8 | buffer[11]
+				.data_length=buffer[10] | buffer[11] << 8
 		};
 		// Copy in data from socket
-		packet.data.adis_supply_out = buffer[12] << 8 | buffer[13];
-		packet.data.adis_xgyro_out = fix_complement(buffer[14] << 8 | buffer[15], 14);
-		packet.data.adis_ygyro_out = fix_complement(buffer[16] << 8 | buffer[17], 14);
-		packet.data.adis_zgyro_out = fix_complement(buffer[18] << 8 | buffer[19], 14);
-		packet.data.adis_xaccl_out = fix_complement(buffer[20] << 8 | buffer[21], 14);
-		packet.data.adis_yaccl_out = fix_complement(buffer[22] << 8 | buffer[23], 14);
-		packet.data.adis_zaccl_out = fix_complement(buffer[24] << 8 | buffer[25], 14);
-		packet.data.adis_xmagn_out = fix_complement(buffer[26] << 8 | buffer[27], 14);
-		packet.data.adis_ymagn_out = fix_complement(buffer[28] << 8 | buffer[29], 14);
-		packet.data.adis_zmagn_out = fix_complement(buffer[30] << 8 | buffer[31], 14);
-		packet.data.adis_temp_out = fix_complement(buffer[32] << 8 | buffer[33], 12);
-		packet.data.adis_aux_adc = buffer[34] << 8 | buffer[35];
+		packet.data.adis_supply_out = buffer[12] | buffer[13] << 8;
+		packet.data.adis_xgyro_out = fix_complement(buffer[14] | buffer[15] << 8, 14);
+		packet.data.adis_ygyro_out = fix_complement(buffer[16] | buffer[17] << 8, 14);
+		packet.data.adis_zgyro_out = fix_complement(buffer[18] | buffer[19] << 8, 14);
+		packet.data.adis_xaccl_out = fix_complement(buffer[20] | buffer[21] << 8, 14);
+		packet.data.adis_yaccl_out = fix_complement(buffer[22] | buffer[23] << 8, 14);
+		packet.data.adis_zaccl_out = fix_complement(buffer[24] | buffer[25] << 8, 14);
+		packet.data.adis_xmagn_out = fix_complement(buffer[26] | buffer[27] << 8, 14);
+		packet.data.adis_ymagn_out = fix_complement(buffer[28] | buffer[29] << 8, 14);
+		packet.data.adis_zmagn_out = fix_complement(buffer[30] | buffer[31] << 8, 14);
+		packet.data.adis_temp_out = fix_complement(buffer[32] | buffer[33] << 8, 12);
+		packet.data.adis_aux_adc = buffer[34] | buffer[35];
 //		unsigned char* data_section = &(buffer[12]);
 //		memcpy(&packet.data, data_section, sizeof(ADIS16405BurstData));
 //		printf("%d, %d, %d\n", packet.data.adis_xgyro_out, packet.data.adis_xaccl_out, packet.data.adis_temp_out);
