71050a12d6168dd05919f02faea4c73c793a5648
TheoH <Theo0x48@gmail.com>
2013-06-26 02:03:56 -0700

    Fixed bugs in roll path

--- rollcontrol.c
+++ rollcontrol.c
@@ -12,21 +12,21 @@ void rollcontrol_init(void){
 uint16_t scale_accel(int16_t acc){
 	const double shift = 34500 + 1374.05;
 	const double scale = 4.57559; // (0.003 g/adis_lsb) * (1374.05 rc_counts/g)
-	return (uint16_t)(((double)-acc)/scale + shift);
+	return (uint16_t)(((double)-acc)*scale + shift);
 }
 
 uint16_t scale_gyro(int16_t gyr){
 	const double shift = 29400;
 	const double scale = 2.1845;// (0.05 deg/sec/adislsb) * (43.69 rc_counts/deg/sec)
-	return (uint16_t)(((double)-gyr)/scale + shift);
+	return (uint16_t)(((double)-gyr)*scale + shift);
 }
 void rc_receive_imu(ADISMessage * imu){
 	RC_INPUT_STRUCT_TYPE input;
 	RC_OUTPUT_STRUCT_TYPE output;
 
-	input.u16RawAccelerometerADC = scale_accel(imu->data.adis_zaccl_out);
-	input.u16RawRateGyroADC = scale_gyro(imu->data.adis_zgyro_out);
-	input.u8IsLaunchDetected = !launch;
+	input.u16RawAccelerometerADC = scale_accel(imu->data.adis_xaccl_out);
+	input.u16RawRateGyroADC = scale_gyro(imu->data.adis_xgyro_out);
+	input.u8IsLaunchDetected = launch;
 	rc_step(&input, &output);
 	output.u8ServoDisableFlag=0;
 
--- rollservo.c
+++ rollservo.c
@@ -16,8 +16,8 @@ void rollservo_final(void){
 
 void rs_receive_adj(RollServoMessage* adj){
 	char data[3];
-	data[0] = (0xff00 & adj->u16ServoPulseWidthBin14) >> 8;
-	data[1] = (0xff & adj->u16ServoPulseWidthBin14);
+	data[0] = (0xff & adj->u16ServoPulseWidthBin14);
+	data[1] = (0xff00 & adj->u16ServoPulseWidthBin14) >> 8;
 	data[2] = adj->u8ServoDisableFlag;
 	sendto_socket(sd, data, sizeof(data), ROLL_IP, ROLL_TX_PORT);
 }
