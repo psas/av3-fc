8e3f12181c2766445abc059e296640c803f43c1d
Nathan Bergey <nathan.bergey@gmail.com>
2013-11-01 17:21:25 -0700

    direct copy of buffer to ADIS struct, needs fix on ADIS board

--- adis.c
+++ adis.c
@@ -29,7 +29,7 @@ int16_t fix_complement(uint16_t val, int bits) {
 
 void adis_raw_in(unsigned char *buffer, int len, unsigned char* timestamp) {
 	//FIXME: this works right now, but is not actually right
-	if(len == sizeof(ADISMessage)){
+	if(len == sizeof(ADIS16405BurstData)){
 		// Build message header
 		ADISMessage packet ={
 				.ID={"ADIS"},
@@ -39,22 +39,7 @@ void adis_raw_in(unsigned char *buffer, int len, unsigned char* timestamp) {
 				.data_length=sizeof(ADIS16405BurstData)
 		};
 		// Copy in data from socket
-		packet.data.adis_supply_out = buffer[12] | buffer[13] << 8;
-		packet.data.adis_xgyro_out = fix_complement(buffer[14] | buffer[15] << 8, 14);
-		packet.data.adis_ygyro_out = fix_complement(buffer[16] | buffer[17] << 8, 14);
-		packet.data.adis_zgyro_out = fix_complement(buffer[18] | buffer[19] << 8, 14);
-		packet.data.adis_xaccl_out = fix_complement(buffer[20] | buffer[21] << 8, 14);
-		packet.data.adis_yaccl_out = fix_complement(buffer[22] | buffer[23] << 8, 14);
-		packet.data.adis_zaccl_out = fix_complement(buffer[24] | buffer[25] << 8, 14);
-		packet.data.adis_xmagn_out = fix_complement(buffer[26] | buffer[27] << 8, 14);
-		packet.data.adis_ymagn_out = fix_complement(buffer[28] | buffer[29] << 8, 14);
-		packet.data.adis_zmagn_out = fix_complement(buffer[30] | buffer[31] << 8, 14);
-		packet.data.adis_temp_out = fix_complement(buffer[32] | buffer[33] << 8, 12);
-		packet.data.adis_aux_adc = buffer[34] | buffer[35];
-//		unsigned char* data_section = &(buffer[12]);
-//		memcpy(&packet.data, data_section, sizeof(ADIS16405BurstData));
-//		printf("%d, %d, %d\n", packet.data.adis_xgyro_out, packet.data.adis_xaccl_out, packet.data.adis_temp_out);
-		// Send data out
+		memcpy(&packet.data, buffer, sizeof(ADIS16405BurstData));
 
 		adis_data_out(&packet);
 	}
