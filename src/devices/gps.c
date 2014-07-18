2a3ba77859c43a1cab16726d0f73e046f0b71d0a
TheoH <Theo0x48@gmail.com>
2014-07-17 19:09:57 -0700

    Added GPS almanac printing

--- src/devices/gps.c
+++ src/devices/gps.c
@@ -12,19 +12,24 @@ void cots_raw_in(unsigned char* buffer, unsigned int len, unsigned char* timesta
 	printf("%d, ", (uint16_t)buffer[4]);
 
 	GPSFixData data;
+	GPSAlmanacData alm;
 	switch(len - 11){
 	case sizeof(GPSFixData):
 		memcpy(&data, buffer + 8, sizeof(GPSFixData));
-		printf("GPSFixData, NOS:%d, NAVM: %d, LAT: %f, LON: %f, H: %f, VN: %f, VE: %f, VU: %f", data.gps1_num_of_sats, data.gps1_nav_mode, data.gps1_latitude, data.gps1_longitude, data.gps1_height, data.gps1_vnorth, data.gps1_veast, data.gps1_vup);
+		printf("GPSFixData, NOS:%d, NAVM: %d, LAT: %f, LON: %f, H: %f, VN: %f, VE: %f, VU: %f",
+				data.gps1_num_of_sats,
+				data.gps1_nav_mode,
+				data.gps1_latitude,
+				data.gps1_longitude,
+				data.gps1_height,
+				data.gps1_vnorth,
+				data.gps1_veast,
+				data.gps1_vup);
 		break;
 
 	case sizeof(GPSFixQualityData):
 		printf("GPSFixQualityData, ");
 			break;
-
-	case sizeof(GPS2Message):
-		printf("GPS2Message, ");
-		break;
 
 	case sizeof(GPSWAASMessageData):
 		printf("GPSWAASMessageData, ");
@@ -38,24 +43,13 @@ void cots_raw_in(unsigned char* buffer, unsigned int len, unsigned char* timesta
 		printf("GPSEphemerisData, ");
 		break;
 
-	case sizeof(GPS95Message):
-		printf("GPS95Message, ");
-		break;
-
 	case sizeof(GPSPsudorangeData):
 		printf("GPSPsudorangeData, ");
 		break;
 
-	case sizeof(GPS96Message):
-		printf("GPS96Message, ");
-		break;
-
 	case sizeof(GPSAlmanacData):
-		printf("GPSAlmanacData, ");
-		break;
-
-	case sizeof(GPS98Message):
-		printf("GPS98Message, ");
+		memcpy(&alm, buffer + 8, sizeof(GPSAlmanacData));
+		printf("GPSAlmanacData, LALM:%d, ", alm.gps98_last_alman);
 		break;
 
 	case sizeof(GPSSatelliteData):
