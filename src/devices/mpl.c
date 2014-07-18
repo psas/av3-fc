509d32ad6c95654e4f07bdc8ede5ff6b0b179c5c
TheoH <Theo0x48@gmail.com>
2014-07-18 04:57:32 -0700

    Fixed MPL logging

--- src/devices/mpl.c
+++ src/devices/mpl.c
@@ -1,24 +1,19 @@
 #include <string.h>
 #include <stdint.h>
+#include "utils_sockets.h"
 #include "mpl.h"
 
 void mpl_raw_in(unsigned char *buffer, unsigned int len, unsigned char* timestamp) {
-	if(len == sizeof(MPLMessage)){
-		// Build message header
+	if(len == sizeof(MPLData)){
 		MPLMessage packet ={
-				.ID={"MPL3"},
-				.timestamp={(uint8_t)timestamp[0], (uint8_t)timestamp[1],
-						    (uint8_t)timestamp[2], (uint8_t)timestamp[3],
-						    (uint8_t)timestamp[4], (uint8_t)timestamp[5]},
-				.data_length=buffer[10] | buffer[11] << 8
+			.ID={"MPL3"},
+			.timestamp={(uint8_t)timestamp[0], (uint8_t)timestamp[1],
+			            (uint8_t)timestamp[2], (uint8_t)timestamp[3],
+			            (uint8_t)timestamp[4], (uint8_t)timestamp[5]},
+			.data_length=htonl(len)
 		};
-		// Copy in data from socket
-		packet.data = buffer;
-//		unsigned char* data_section = &(buffer[12]);
-//		memcpy(&packet.data, data_section, sizeof(MPL_burst_data));
+		memcpy(&packet.data, buffer, len);
 
-		// Send data out
 		mpl_data_out(&packet);
 	}
-	// TODO: else log error?
 }
--- src/devices/mpl.h
+++ src/devices/mpl.h
@@ -8,15 +8,19 @@
 #include <stdint.h>
 
 typedef struct {
+	uint32_t pressure;
+	uint16_t temperature;
+} __attribute__((packed)) MPLData;
+
+typedef struct {
 	char     ID[4];
 	uint8_t  timestamp[6];
 	uint16_t data_length;
-	unsigned char * data;
-}__attribute__((packed)) MPLMessage;
+	MPLData data;
+} __attribute__((packed)) MPLMessage;
 
-extern void mpl_data_out(MPLMessage*); // [miml:sender]
-
-extern void mpl_raw_in(unsigned char*, unsigned int, unsigned char*); // [miml:reciever]
+void mpl_data_out(MPLMessage*);
+void mpl_raw_in(unsigned char*, unsigned int, unsigned char*);
 
 
 #endif /* MPL_H_ */
