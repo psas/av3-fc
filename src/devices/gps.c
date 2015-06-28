48f243c93a5a3605bd8ba060b7332a9bc861917c
Nathan Bergey <nathan.bergey@gmail.com>
2015-06-28 16:00:35 -0700

    GPS: only parse COTS GPS message if length matches

--- src/devices/gps.c
+++ src/devices/gps.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
-
 #include "gps.h"
 
 
@@ -11,16 +10,21 @@
  */
 void cots_raw_in(unsigned char *data, unsigned int len, unsigned char *timestamp)
 {
-	// Construct a venus message
-	V6NAMessage p = {
-		.ID = "V6NA",
-		.data_length = sizeof(Venus6FixData),
-	};
-	memcpy(&p.timestamp, timestamp, sizeof(p.timestamp));
+	if (len == sizeof(Venus6FixData)) {
+		// Construct a venus message
+		V6NAMessage p = {
+			.ID = "V6NA",
+			.data_length = sizeof(Venus6FixData),
+		};
+		memcpy(&p.timestamp, timestamp, sizeof(p.timestamp));
 
-	// Copy in message off the wire as data
-	memcpy(&p.data, data, len);
+		// Copy in message off the wire as data
+		memcpy(&p.data, data, sizeof(Venus6FixData));
 
-	// Send data out to consumers
-	gps_data_out(&p);
+		// Send data out to consumers
+		gps_data_out(&p);
+    }
+	else {
+		perror("Malformed GPS COTS Message");
+	}
 }
